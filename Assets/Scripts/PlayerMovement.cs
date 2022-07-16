402a2d07a40bb9c3f7de4ed5ec5042a9a8ab8532
Gabriel Gallegos <gabriel.gallegos@uabc.edu.mx>
2022-07-16 01:20:27 -0700

    Salto reparado
    Se arregló la validación de que solo se pueda saltar si está en el suelo.
    Ahora funciona perfecto.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Variables p�blicas
+    // Variables p�blicas
     public Rigidbody2D rbody;
     public float speed;
     public float jumpForce;
-    public LayerMask layer;
-    public Transform pies;
+    //public Transform pies;
 
-    //Variables boleanas
-    public bool enSuelo;
+    public Vector2 pies;
+    public LayerMask layerPiso;
+    public float radioColision;
+
+    // Variables boleanas
+    public bool enSuelo = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,38 +27,34 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         CheckGround();
-        //SALTO
-        saltando();
+        // SALTO
+        // saltando();
        
-        //MOVIMIENTO HACIA LA DERECHA
-        //this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, this.GetComponent<Rigidbody2D>().velocity.y);
+        // MOVIMIENTO HACIA LA DERECHA
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, this.GetComponent<Rigidbody2D>().velocity.y);
     }
 
-    private void saltando()
+    private void OnJump()
     {
-        if (Input.GetKeyDown("space"))
+        Debug.Log("Jump!");
+        if (enSuelo == true)
         {
-            if (enSuelo)
-            {
-                Debug.Log("Jump!");
-                rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
-            }
-            
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
         }
     }
 
     private void CheckGround()
     {
-
-        RaycastHit2D hit = Physics2D.Raycast(pies.position, Vector2.down, 0.5f);
-        Debug.DrawRay(pies.position, Vector2.down, Color.yellow, 0.5f);
-        if (hit.collider != null)
-        {
-            enSuelo = true;
-        }
-        else
-        {
-            enSuelo = false;
-        }
+        enSuelo = Physics2D.OverlapCircle((Vector2)transform.position + pies, radioColision, layerPiso);
+        //RaycastHit2D hit = Physics2D.Raycast(pies.position, Vector2.down, 0.5f);
+        //Debug.DrawRay(pies.position, Vector2.down, Color.yellow, 0.5f);
+        //if (hit.collider != null)
+        //{
+        //    enSuelo = true;
+        //}
+        //else
+        //{
+        //    enSuelo = false;
+        //}
     }
 }
