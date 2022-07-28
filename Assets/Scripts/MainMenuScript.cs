0475eea67f348c3f632e03bb83b4356c458473d1
Gabriel Gallegos <gabriel.gallegos@uabc.edu.mx>
2022-07-28 02:09:41 -0700

    Error solucionado
    Se arregló el error del menu que hacía que el menu principal no se mostrara bien.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void Start()
+    public void Play()
     {
         SceneManager.LoadScene("MainScene");
     }
